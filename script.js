9fc21dba2b0011d0346cdfdb30fac032d3004beb
Saurav <singhsaurav062@gmail.com>
2024-01-06 16:43:10 +0530

    commit

--- script.js
+++ script.js
@@ -10,78 +10,80 @@ function changeIP() {
   // Update video sources based on class
   var videoImages = document.getElementsByClassName("video-image");
   for (var i = 0; i < videoImages.length; i++) {
-    var currip1 = (videoImages[i].id == "browser_video1") ? ip1 : ip2;
+    var currip1 = videoImages[i].id == "browser_video1" ? ip1 : ip2;
     videoImages[i].src = "https://" + currip1 + ":8080/video";
     videoImages[i].onerror = function () {
       this.style.display = "none"; // Hide the image on error
-      console.log(videoImages[i],"hidden")
-      
+      console.log(this, "hidden");
     };
     videoImages[i].onload = function () {
       this.style.display = "block"; // Show the image when successfully loaded
-      console.log(videoImages[i],"opened")
+      console.log(this, "opened");
     };
   }
 
   var videoImagesIos = document.getElementsByClassName("video-image-ios");
   for (var i = 0; i < videoImagesIos.length; i++) {
-    var currip2 = (videoImagesIos[i].id == "browser_video2") ? ip1 : ip2;
+    var currip2 = videoImagesIos[i].id == "browser_video2" ? ip1 : ip2;
     videoImagesIos[i].src = "https://" + currip2 + ":80/video.mjpg";
     videoImagesIos[i].onerror = function () {
       this.style.display = "none"; // Hide the image on error
-      console.log(videoImages[i],"hidden")
+      console.log(this, "hidden");
     };
     videoImagesIos[i].onload = function () {
       this.style.display = "block"; // Show the image when successfully loaded
-      console.log(videoImages[0],"opened")
+      console.log(this, "opened");
     };
   }
 }
 
-
-
 let mediaRecorder;
-    let recordedChunks = [];
+let recordedChunks = [];
 
-    const startBtn = document.getElementById('startBtn');
-    const stopBtn = document.getElementById('stopBtn');
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
 
-    startBtn.addEventListener('click', startRecording);
-    stopBtn.addEventListener('click', stopRecording);
+startBtn.addEventListener("click", startRecording);
+stopBtn.addEventListener("click", stopRecording);
 
-    async function startRecording() {
-        const stream = await navigator.mediaDevices.getDisplayMedia({
-            video: { mediaSource: 'screen' },
-        });
+async function startRecording() {
+  const stream = await navigator.mediaDevices.getDisplayMedia({
+    video: { mediaSource: "screen" },
+  });
 
-        mediaRecorder = new MediaRecorder(stream);
+  // Modify the URL to use HTTPS
+  const streamURL = stream.getTracks()[0].getSettings().video;
+  const secureStreamURL = streamURL.replace("http://", "https://");
 
-        mediaRecorder.ondataavailable = event => {
-            if (event.data.size > 0) {
-                recordedChunks.push(event.data);
-            }
-        };
+  mediaRecorder = new MediaRecorder(stream);
 
-        mediaRecorder.onstop = () => {
-            const blob = new Blob(recordedChunks, { type: 'video/mp4' });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'screen-recording.mp4';
-            document.body.appendChild(a);
-            a.click();
-            recordedChunks = [];
-            document.body.removeChild(a);
-        };
-
-        startBtn.disabled = true;
-        stopBtn.disabled = false;
-
-        mediaRecorder.start();
+  mediaRecorder.ondataavailable = (event) => {
+    if (event.data.size > 0) {
+      recordedChunks.push(event.data);
     }
+  };
 
-    function stopRecording() {
-        mediaRecorder.stop();
-        startBtn.disabled = false;
-        stopBtn.disabled = true;
-    }
+  mediaRecorder.onstop = () => {
+    const blob = new Blob(recordedChunks, { type: "video/mp4" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "screen-recording.mp4";
+    document.body.appendChild(a);
+    a.click();
+    recordedChunks = [];
+    document.body.removeChild(a);
+  };
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  // Use the secureStreamURL for recording
+  mediaRecorder.start();
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
